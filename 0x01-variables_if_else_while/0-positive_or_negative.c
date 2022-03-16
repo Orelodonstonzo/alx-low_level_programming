21947e38b4ff5f6ddcf3e96ea8bdc7558bc54ff8
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-16 04:21:36 -0700

    Loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  ​* main - Entry point, prints out
  ​* random numbers
- ​*Return: Always 0 (Success)
+ ​* Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -18,6 +18,5 @@ else if (n == 0)
 printf("%d is zero\n", n);
 else
 printf("%d is negative\n", n);
-return (0)
-;
+return (0);
 }
