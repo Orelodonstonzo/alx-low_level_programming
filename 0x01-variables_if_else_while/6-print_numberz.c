e625bfdd34bb68ff9eb23656841e3ecbaee657a9
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-18 03:26:37 -0700

    Dot

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 /**
 * main - Entry point, prints the single
 * digits in base ten
-* 
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 int i;
 for (i = 0 ; i < 10 ; i++)
+putchar(i + '0');
 putchar ('\n');
 return (0);
 }
