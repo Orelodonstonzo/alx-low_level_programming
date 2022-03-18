88284999b3c0530b1c6deab6037c613537072354
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-18 05:28:11 -0700

    Lint

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,33 @@ for (j = i + 1 ; j <= 9 ; j++)
 {
 putchar (i + '0');
 putchar (j +'0');
-if ( i != 8)
+if (i != 8)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+putchar ('\n');
+return (0);
+}
+#include <stdio.h>
+/**
+* main - Entry point, prints all possible different
+* combinations of two digits
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j;
+
+for (i = 0 ; i <= 8 ; i++)
+{
+for (j = i + 1 ; j <= 9 ; j++)
+{
+putchar (i + '0');
+putchar (j +'0');
+if (i != 8)
 {
 putchar (',');
 putchar (' ');
