398f5c21a41b890b497aa7a045f79b84aa922d72
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-17 19:15:11 -0700

    Port

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* 
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-char ch = 'a' ;
+char ch = 'a';
 for (; ch <= 'z' ; ch++)
 {
 {
-while(ch == 'e' || ch =='q')
+while (ch == 'e' || ch == 'q')
 ch++;
 }
 putchar(ch);
