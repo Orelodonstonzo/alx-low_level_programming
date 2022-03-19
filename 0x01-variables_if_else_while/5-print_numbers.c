da07446219f80959b125613db7f085a178dc4759
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-19 06:50:04 -0700

    Pin

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,14 @@
 /**
 * main - Entry point, prints out all
 * single digit in base ten
-* 
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 int i;
+
 for (i = 0 ; i < 10 ; i++)
-printf("%d", i);
-putchar(n);
+printf ("%d", i);
+putchar ('\n');
 return (0);
 }
