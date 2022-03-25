01d71ae02874c2a6ffb08e17b20943ba1518b949
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-25 01:42:54 +0000

    Frodo

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* _strncat - a function that concatenates two strings
+* _strncat - a function that concatenates
+* two strings
 * up to a position n.
 * @dest: the string to copy into
 * @src: the string to copy
@@ -10,6 +11,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
+
 for (i = 0; dest[i] != '\0'; i++)
 {
 }
