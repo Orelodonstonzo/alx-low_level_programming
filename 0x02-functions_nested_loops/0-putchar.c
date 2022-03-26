57d8937091da2429b7c1fac279cbf80c0917f10d
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-26 12:21:07 +0000

    Plause

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,10 @@
 * Program that prints _putchar, followed by a new line.
 * Return: Always 0 (Success)
 */
- 
+
 int main(void)
 {
-char s[ ] = "_putchar";
+char s[] = "_putchar";
 int i = 0;
 
 while (s[i] != 0)
