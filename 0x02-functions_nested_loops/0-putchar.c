2816fb2645212232f88ba40c9e386b670abc3488
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-26 11:22:30 +0000

    Peel

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - check the code for ALX School students
+* Program that prints _putchar, followed by a new line.
+* Return: Always 0 (Success)
+*/
+ 
+int main(void)
+{
+char s[ ] = "_putchar";
+int i = 0;
+
+while (s[i] != 0)
+{
+_putchar (s[i]);
+i++;
+}
+_putchar (10);
+}
