954f729b78b99a67db7562e92a947024355a8493
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-29 11:58:31 +0000

    Toyota

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,13 +10,12 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i, j, c;
-
 i = 0;
 c = 0;
 while (haystack[i] != '\0')
 {
 j = 0;
-while (needle[j + c] != '\0' &&haystack[i + c] != '\0' &&needle[j + c] == haystack[i + c])
+while (needle[j + c] != '\0' && haystack[i + c] != '\0' && needle[j + c] == haystack[i + c])
 {
 if (haystack[i + c] != needle[j + c])
 break;
