cadae2d1f2de7ae9477a61e2015181f14b2f79ec
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-29 18:25:01 +0000

    Porshe

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strncat - function that concatenates two strings
+*_strncat - function that concatenates two strings
 * @dest: string
 * @src: string
 * @n: number of element to concatenate in
