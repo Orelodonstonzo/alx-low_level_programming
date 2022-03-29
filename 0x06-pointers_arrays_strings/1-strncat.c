3d3bd648fe9eb21a8076fa0d9e41446b5c9a87a6
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-03-29 18:37:13 +0000

    Porshe

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,6 @@
 * @n: number of element to concatenate in
 * Return: a pointer to the resulting 'dest'
 */
-
 char *_strncat(char *dest, char *src, int n)
 {
 int i, c;
@@ -17,5 +16,5 @@ for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 {
 dest[i] = src[c];
 }
-return (dest);
+return(dest);
 }
