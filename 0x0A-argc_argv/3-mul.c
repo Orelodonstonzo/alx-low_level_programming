cd125d15dfe2f122f953c083d60fa1c24f9f6886
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-04-02 01:22:30 +0000

    Lincon Navigator

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - a program that multiplies two numbers
+* @argc: the argument counter
+* @argv: the argument values
+* Return: always 0 (succuss)
+*/
+int main(int argc, char **argv)
+{
+int num1, num2, result;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+printf("%d\n", result);
+return (0);
+}
+}
