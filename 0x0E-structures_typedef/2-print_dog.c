93134a7eb95f8df0e7484df3eb79574ca8291e3e
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-04-15 18:43:03 +0000

    Innoson

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,16 +6,29 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-
 if (d->name == NULL)
+{
 printf("name: %s\n", "(nil)");
-if (d->age <= 0)
-printf("age: %d\n", 0);
-if (d->owner == NULL)
-printf("owner: %s\n", "(nil)");
-
+}
+else
+{
 printf("name: %s\n", d->name);
+}
+if (d->age == 0)
+{
+printf("age: %s\n", "0");
+}
+else
+{
 printf("age: %f\n", d->age);
+}
+if (d->owner == NULL)
+{
+printf("owner: %s\n", "(nil)");
+}
+else
+{
 printf("owner: %s\n", d->owner);
+}
 return;
 }
