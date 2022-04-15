308e6775313e9b38fade9ba488f450717cca3459
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-04-15 17:32:24 +0000

    Innoson

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,8 @@ return;
 
 if (d->name == NULL)
 printf("name: %s\n", "(nil)");
-if (d->age == 0)
-printf("age: %s\n", "(0)");
+if (d->age <= 0)
+printf("age: %f\n", "(0)");
 if (d->owner == NULL)
 printf("owner: %s\n", "(nil)");
 
