549bffa22bb702e2630c231f43910bcf5cb945c1
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-04-15 15:44:43 +0000

    Honda

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,7 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-{
+
 if (d->name == NULL)
 printf("name: %s\n", "(nil)");
 if (d->age == 0)
@@ -18,5 +18,4 @@ printf("name: %s\n", d->name);
 printf("age: %f\n", d->age);
 printf("owner: %s\n", d->owner);
 return;
-}
 }
