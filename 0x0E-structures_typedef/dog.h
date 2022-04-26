c326aa2fffff7dd2ac6d7d1cc39a1bb0cabb2cd5
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-04-26 15:13:08 +0000

    Gates

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-struct dog                                              {                                                       char *name;                                             float age;                                              char *owner;                                            };                                                      void init_dog(struct dog *d, char *name, float age, char *owner);                                               void print_dog(struct dog *d);
-#include <stdio.h>
-
-typedef struct
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog_t;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /*DOG_H*/
