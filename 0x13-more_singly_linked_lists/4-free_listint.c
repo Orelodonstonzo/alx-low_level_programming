f08f8fbaae1da066bd8a88fb8897d87bf53cd412
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-04-28 01:16:27 +0000

    Penguine

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -18,3 +18,23 @@ free(head->next);
 }
 free(head);
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* free_listint - a function that frees a listint_t list
+* @head: head node
+* Return: void
+*/
+void free_listint(listint_t *head)
+{
+if (head == NULL)
+return;
+if (head->next != NULL)
+{
+free(head->next);
+}
+free(head);
+}
