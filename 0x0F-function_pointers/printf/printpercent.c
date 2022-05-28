d6afe12c29f79bda2f6d631f480260f3dea643a8
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-05-28 12:37:28 +0000

    again

---
+++ 0x0F-function_pointers/printf/printpercent.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+* printpercent - print %
+* @format: format
+* @pa: va_list
+* return: number of characters printed
+*/
+int printpercent(char *format, va_list pa)
+{
+(void)format;
+(void)pa;
+_putchar('%');
+return (1);
+}
